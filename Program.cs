d650e4f4de03f98d5ba6480c2c4ccb1176108312
David <113856260+Davidkovel@users.noreply.github.com>
2024-11-23 15:28:04 +0000

    hw task 3 is done

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using ClassApplication.Enteties;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        try
+        {
+            DateReceivedPassport dateReceivedPassport = new DateReceivedPassport(22, 09, 2024);
+            Passport passport = new Passport("123443218", "David", ref dateReceivedPassport);
+            Console.WriteLine("Паспорт успішно створено");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"Помилка: {ex.Message}");
+        }
+    }
+}
+
+/*
+Паспорт успішно створено
+
+C:\Users\David\source\repos\ConsoleApp1\ConsoleApp1\bin\Debug\net8.0\ConsoleApp1.exe (процесс 13468) завершил работу с кодом 0 (0x0).
+Нажмите любую клавишу, чтобы закрыть это окно…
+*/
---
+++ enteties/Passport.cs
@@ -0,0 +1,37 @@
+﻿using ClassApplication.Enteties;
+
+class Passport
+{
+    public string PassportNumber { get; private set; }
+    public string FullName { get; private set; }
+    public DateReceivedPassport receivedPassport { get; private set; }
+
+    public Passport()
+    {
+        this.PassportNumber = string.Empty;
+        this.FullName = string.Empty;
+        this.receivedPassport = new DateReceivedPassport();
+    }
+
+    public Passport(string passportNumber, string fullName, ref DateReceivedPassport receivedPassport) : this()
+    {
+        if (string.IsNullOrWhiteSpace(passportNumber) || passportNumber.Length != 9)
+        {
+            throw new ArgumentException("Номер паспорта повинен містити 9 символів.");
+        }
+
+        if (string.IsNullOrWhiteSpace(fullName))
+        {
+            throw new ArgumentException("ПІБ власника не може бути порожнім.");
+        }
+
+        if (receivedPassport.ToDateTime() > DateTime.Now)
+        {
+            throw new ArgumentException("Дата видачі не може бути в майбутньому.");
+        }
+
+        PassportNumber = passportNumber;
+        FullName = fullName;
+        receivedPassport = receivedPassport;
+    }
+}
